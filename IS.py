5b28d046d1ddbb3b24ea1ec1f6ae424af1951675
bookjan <su1221jan@gmail.com>
2015-09-17 01:13:10 +0800

    for testing

--- IS.py
+++ IS.py
@@ -41,13 +41,18 @@ from json import load, JSONEncoder
 
 import re
 
-from flask import Flask, Response, jsonify
+from flask import Flask, Response, jsonify, request, redirect, url_for, send_from_directory
+
+from werkzeug import secure_filename
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 APP_JSONFILES = os.path.join(APP_ROOT, 'Geojsonfiles')
 
 # Create Flask application
 app = Flask(__name__)
+
+app.config['UPLOAD_FOLDER'] = 'upload/'
+app.config['ALLOWED_EXTENSIONS'] = set(['json'])
 
 @app.route('/')
 def index():
@@ -96,6 +101,25 @@ def lookup_citylist():
 
     return jsonify(results=city_sets)
 
+
+# Route that will process the file upload
+@app.route('/upload', methods=['POST'])
+def upload():
+    # Get the name of the uploaded file
+    file = request.files['file']
+    # Check if the file is one of the allowed types/extensions
+    if file and allowed_file(file.filename):
+        # Make the filename safe, remove unsupported chars
+        filename = secure_filename(file.filename)
+        # Move the file form the temporal folder to
+        # the upload folder we setup
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+        # Redirect the user to the uploaded_file route, which
+        # will basicaly show on the browser the uploaded file
+        return redirect(url_for('uploaded_file',
+                                filename=filename))
+
+
 def merge_geojsons(json_file):
 
     injson = load(json_file)
@@ -107,6 +131,20 @@ def merge_geojsons(json_file):
             raise Exception('Sorry, "%s" does not look like GeoJSON' % infile)
 
     return injson['features']
+
+# For a given file, return whether it's an allowed type or not
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1] in app.config['ALLOWED_EXTENSIONS']
+
+# This route is expecting a parameter containing the name
+# of a file. Then it will locate that file on the upload
+# directory and show it on the browser, so if the user uploads
+# an image, that image is going to be show after the upload
+@app.route('/uploads/<filename>')
+def uploaded_file(filename):
+    return send_from_directory(app.config['UPLOAD_FOLDER'],
+                               filename)
 
 
 if __name__ == '__main__':
